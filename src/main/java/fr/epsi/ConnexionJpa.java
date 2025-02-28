896c018b5c3aab531b582a47ad96a9c69b38232e
Alexia <al.nicoleau27@gmail.com>
2025-02-28 11:47:04 +0100

    TP1

---
+++ src/main/java/fr/epsi/ConnexionJpa.java
@@ -0,0 +1,15 @@
+package fr.epsi;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class ConnexionJpa {
+    public static void main(String[] args) {
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpa");
+             EntityManager em = emf.createEntityManager()) {
+
+            System.out.println("Ça marche!!!");
+        }
+    }
+}
